e81222bc61d3c65e0a9cfe65afbe4d3bf915b8a6
Mitsuki Tahara <e245748@ie.u-ryukyu.ac.jp>
2024-11-14 20:40:39 +0900

    4th commit

--- app/src/main/java/jp/ac/uryukyu/ie/e245748/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245748/Enemy.java
@@ -35,10 +35,8 @@ public class Enemy {
      */
     public void attack(Hero hero){
         int damage = (int)(Math.random() * attack);
-        if (dead == false){
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
-            hero.wounded(damage);
-        }        
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+        hero.wounded(damage);
     }
 
     /**
